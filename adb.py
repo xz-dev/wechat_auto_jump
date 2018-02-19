54fccec539f6f4c895d2d3c3b98e7a634264c0b1
galaxy <1054855541@qq.com>
2018-02-19 22:19:20 +0800

    carry out the screenshot module

--- adb.py
+++ adb.py
@@ -1,1 +1,16 @@
+"""调用ADB"""
+import os
 
+def adb_sc():
+    """ADB截图"""
+    os.chdir('adb')
+    out = os.popen('adb devices').read() # 检查设备状态
+    print(out)
+    if 'device' not in str.split(out):
+        print('请检查设备状态\n')
+        return(0)
+    else:
+        os.popen('adb shell screencap /sdcard/screen.png') # ADB工具截图
+        os.popen('adb pull /sdcard/screen.png ..') # 拉取截图至项目根目录
+        print('初始截图完成')
+        return(1)
