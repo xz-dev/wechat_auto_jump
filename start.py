dd75297bdcb77aa5bc60512fa81be2445cfb30be
galaxy <1054855541@qq.com>
2018-02-20 23:30:04 +0800

    use subprocess to run ADB and use re to analysis ADB's results

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
-"""启动文件"""
+"""starter"""
 import msvcrt
 import adb
 
-out = adb.adb_sc() # 调用ADB模块截图
-if out == 0:
-    print('按任意键结束')
+out = adb.adb_sc() # screenshots
+if out == 0: # exit the program
+    print('Press any key to continue . . .')
     msvcrt.getch()
     exit()
