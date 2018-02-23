db315413285355f442a0a1ff9754f51c3f2c5956
Deemo-x <1054855541@qq.com>
2018-02-24 00:49:41 +0800

    platform recognize

--- start.py
+++ start.py
@@ -9,15 +9,21 @@ def main():
     """Main function"""
     out = adb_utils.adb_sc()  # screenshots
     if out:
+        print("Wait...")
+        platform_position = 0
         img_proc.edge_detection()
         piece_position =  img_proc.find_piece()
         img = cv2.imread('screen.png')
         cropped_piece = img[:,piece_position[1]:piece_position[1] + piece_position[2]]
+        img[piece_position[0]] = (0, 0, 0)
         cv2.imwrite('piece.png', cropped_piece)
         if piece_position[1] < cv2.imread('screen.png').shape[1] / 2:
-            img_proc.img_cropped(left = piece_position[1] + piece_position[2])
+           platform_position =  img_proc.find_platform(left = piece_position[1] + piece_position[2])
         else:
-            img_proc.img_cropped(right = piece_position[1])
+            platform_position = img_proc.find_platform(right = piece_position[1])
+        cropped_platform = img[:,:]
+        img[platform_position[0]] = (0, 0, 0)
+        cv2.imwrite('platform.png', cropped_platform)
 
 
 if __name__ == '__main__':
