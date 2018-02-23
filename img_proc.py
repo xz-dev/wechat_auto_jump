35c632887bb258b792dd1d68b491039658526d98
Deemo-x <1054855541@qq.com>
2018-02-24 00:49:41 +0800

    platform recognize

--- img_proc.py
+++ img_proc.py
@@ -12,7 +12,7 @@ def edge_detection(read_img = "screen.png", save_name = 'edge.png'):
     img = cv2.imread(read_img) # read image
     cv2.imwrite(save_name, cv2.Canny(img, 75, 100))
 
-def find_edge(read_img = "edge.png", top = 0.80, bottom = 0.30):
+def find_edge(left, right, top = 0.20, bottom = 0.70 , read_img = "edge.png"):
     """
     Get all positions of white pixel
     to reduce the amount of computation
@@ -20,24 +20,20 @@ def find_edge(read_img = "edge.png", top = 0.80, bottom = 0.30):
     img = cv2.imread(read_img) # read image
     height = img.shape[0]
     width = img.shape[1]
-    cv2.imwrite('piece.png', img[ int(height * bottom) : int(height * top), : ])
     edge_positions = []
     img_array=np.array(img)  # Open the image and convert it to a numeric matrix
-    for h in range( int(height * bottom), int(height * top)): # search range
-        for x in range(0, width):
+    for h in range( int(height * top), int(height * bottom)): # search range
+        for x in range(int(width * left), int(width * right)):
             pixel = img_array[h][x]
             if pixel[0] != 0:
                 edge_positions.append((h, x))
     print(len(edge_positions))
     return  edge_positions
 
-
 def find_piece(read_img = "screen.png"):
     """find position of the piece""" 
     img = cv2.imread(read_img) # read image
     img_array=np.array(img)  # Open the image and convert it to a numeric matrix
-    for temp in range(0, 13):
-        print(img_array[0][temp])
     height = img.shape[0]
     width = img.shape[1]
     n_array = []
@@ -45,7 +41,7 @@ def find_piece(read_img = "screen.png"):
     edge_right = 0
     tmp_h = -1
     broadband_mix = 0
-    edge_positions = find_edge()
+    edge_positions = find_edge(left = 0.25, right = 0.75)
     if edge_positions:
         for z in range(0, len(edge_positions)):
             h = edge_positions[z][0]
@@ -57,33 +53,81 @@ def find_piece(read_img = "screen.png"):
                     and pixel[2] in list(range(55, 60)) + list(range(40, 45)):
                     if h > tmp_h:
                         tmp_h = h
+                        edge_left = (h, x)
                         if edge_right:
                             broadband_array.append((edge_right[0], edge_left[1],
                                              edge_right[1] - edge_left [1]))
-                        edge_left = (h, x)
                     else:
                         edge_right = (h, x)
     for broadband_tmp in broadband_array:
         if not broadband_mix:
-            broadband_mix = broadband_tmp
-        if broadband_mix[2] < broadband_tmp[2]:
-            broadband_mix = broadband_tmp
+            broadband_mix = list(broadband_tmp)
+        if broadband_mix[2] <= broadband_tmp[2]:
+            broadband_mix = list(broadband_tmp)
+    broadband_mix[2] += 1
     print(broadband_mix)
     return broadband_mix
 
-def img_cropped(left = 0, right = 0):
-    print(left)
-    print(right)
+def find_platform(read_img =  'screen.png', left = 0, right = 0):
+    """find position of the platform"""
+    img = cv2.imread(read_img) # read image
+    img_array=np.array(img)  # Open the image and convert it to a numeric matrix
     if not right:
         right = cv2.imread('screen.png').shape[1]
-    print(cv2.imread('screen.png').shape[1])
+    height = img.shape[0]
+    width = img.shape[1]
+    # Get the range size
+    n_array = []
+    broadband_array = []
+    edge_right = 0
+    platform_bgr = (-1, -1, -1)
+    tmp_h = -1
+    broadband_mix = 0
+    edge_positions = find_edge(left = left / width, right = right / width, bottom = 0.50)
+    if edge_positions:
+        for z in range(0, len(edge_positions)):
+            h = edge_positions[z][0]
+            x = edge_positions[z][1]
+            if platform_bgr[0] == -1 \
+                and (img_array[h][x] == img_array[h + 1][x]).all() \
+                and (img_array[h][x] == img_array[h + 2][x]).all() \
+                and (img_array[h][x] == img_array[h + 3][x]).all():
+                    print(h, x)
+                    img[h, x]=(0,0,0)
+                    cv2.imwrite('img.png', img)
+                    platform_bgr = img_array[h][x]
+        for z in range(0, len(edge_positions)):
+            h = edge_positions[z][0]
+            x = edge_positions[z][1]
+            if (platform_bgr == img_array[h][x]).all():
+                if h > tmp_h:
+                    tmp_h = h
+                    edge_left = (h, x)
+                    if edge_right:
+                        broadband_array.append((edge_right[0], edge_left[1],
+                                                edge_right[1] - edge_left [1]))
+                else:
+                    edge_right = (h, x)
+        print(len(broadband_array))
+        for broadband_tmp in broadband_array:
+            if not broadband_mix:
+                broadband_mix = list(broadband_tmp)
+            if broadband_mix[2] <= broadband_tmp[2]:
+                broadband_mix = list(broadband_tmp)
+    print(broadband_mix)
+    broadband_mix[2] += 1
+    return broadband_mix
+
+def img_cropped(left = 0, right = 0):
     """cropped image into 2 parts (score and platform part)"""
+    if not right:
+        right = cv2.imread('screen.png').shape[1]
     img = cv2.imread('screen.png')
     height = img.shape[0]
     width = img.shape[1]
     # Get the picture size
     cropped_score = img[height // 11 : height // 6, 0 : width // 2] # Get score screenshot
-    cropped_pltform = img[height // 6 : height  * 12 // 13,  left : right]
+    cropped_pltform = img[height // 6 : height  * 2 // 3,  left : right]
     cv2.imwrite('score.png', cropped_score)
     cv2.imwrite('pltform.png', cropped_pltform)
 
@@ -93,7 +137,6 @@ def division(img):
     cv2.imwrite('gray.png', gray)
     #kernel = np.ones((3, 3), np.uint8)
     #opening = cv2.morphologyEx(gray)
-
 
 def score_recognition():
     """Score recognition"""
