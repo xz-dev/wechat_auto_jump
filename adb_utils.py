2618f3e1c4fb6c4a1f2e6ebcdeca5ffa35a3c23f
Deemo-x <1054855541@qq.com>
2018-02-24 11:09:29 +0800

    improve algorithm

--- adb_utils.py
+++ adb_utils.py
@@ -14,8 +14,10 @@ def adb_sc():
     else:
         subprocess.run('adb/adb shell screencap /sdcard/screen.png')  # ADB screenshots
         subprocess.run('adb/adb pull /sdcard/screen.png .')  # Copy the screenshot to the project root directory
-        if input("The initial screenshot is completed. \nContinue? (y/n) ") in ['Y', 'y']:  # Whether to start
-            return True
-        else:
-            print("You canceled your operation. ")
-            return False
+        return True
+
+def adb_touch(time, h1 = 500, x1 = 500, h2 = 500, x2 = 500):
+    adb_swipe = 'adb/adb shell  input swipe ' + str(h1) + ' ' + str(x1) + ' ' + str(h2) + ' ' + str(x2) + ' ' + str(time)
+    print(adb_swipe)
+    subprocess.run(adb_swipe)
+   
--- start.py
+++ start.py
@@ -13,17 +13,19 @@ def main():
         platform_position = 0
         img_proc.edge_detection()
         piece_position =  img_proc.find_piece()
-        img = cv2.imread('screen.png')
-        cropped_piece = img[:,piece_position[1]:piece_position[1] + piece_position[2]]
-        img[piece_position[0]] = (0, 0, 0)
-        cv2.imwrite('piece.png', cropped_piece)
-        if piece_position[1] < cv2.imread('screen.png').shape[1] / 2:
-           platform_position =  img_proc.find_platform(left = piece_position[1] + piece_position[2])
-        else:
-            platform_position = img_proc.find_platform(right = piece_position[1])
-        cropped_platform = img[:,:]
-        img[platform_position[0]] = (0, 0, 0)
-        cv2.imwrite('platform.png', cropped_platform)
+        if piece_position:
+            img = cv2.imread('screen.png')
+            if piece_position[1] < img.shape[1] / 2:
+                # if piece on the left, scanning right
+               platform_position =  img_proc.find_platform(left = piece_position[1] + img.shape[1] * 0.03125)
+            else:
+                # if piece on the right, scanning left
+                platform_position = img_proc.find_platform(right = piece_position[1])
+
+            img[piece_position[0], piece_position[1]] = (255, 0, 0)
+            img[platform_position[0], platform_position[1]] = (225, 0, 0)
+            cv2.imwrite('center.png', img)
+            # mark the position in the image
 
 
 if __name__ == '__main__':
