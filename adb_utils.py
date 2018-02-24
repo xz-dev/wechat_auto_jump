2fe854fcac61ef87f5b0ae49724f544336d4a2ae
wirehack <kevinwirehack@gmail.com>
2018-02-24 16:20:21 +0800

    Use numpy functions for faster performance, avoid reload images and reformat code. Please note that one should always write 2 blink lines between functions and imports.

--- adb_utils.py
+++ adb_utils.py
@@ -16,8 +16,8 @@ def adb_sc():
         subprocess.run('adb/adb pull /sdcard/screen.png .')  # Copy the screenshot to the project root directory
         return True
 
-def adb_touch(time, h1 = 500, x1 = 500, h2 = 500, x2 = 500):
+
+def adb_touch(time, h1=500, x1=500, h2=500, x2=500):
     adb_swipe = 'adb/adb shell  input swipe ' + str(h1) + ' ' + str(x1) + ' ' + str(h2) + ' ' + str(x2) + ' ' + str(time)
     print(adb_swipe)
     subprocess.run(adb_swipe)
-   
