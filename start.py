aca6244f7df3e8af8e371b4a5d3c73ab3e1a15d2
Deemo-x <zxz1054855541@Gmail.com>
2018-02-28 00:32:31 +0800

    add json file and perfect no debug mode

--- start.py
+++ start.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 
 import time
+import json
 import random
 
 import adb_utils
@@ -14,39 +15,47 @@ def main():
     i = 0
     out = True
 
+    avatar_img = cv2.imread('images/player.png')
+    avatar_img = cv2.cvtColor(avatar_img, cv2.COLOR_BGR2GRAY)
+
     try:
-        with open('profile/default.txt', 'r') as f:
-            str = f.read().split()
-            phone_model = str[0]
-            debug_mode = str[1]
+        with open('profile/default.json', 'r') as f:
+            configuration = json.load(f)
+            phone_model = configuration["cellType"]
+            debug_mode = configuration["debug"]
     except FileNotFoundError:
         phone_model = input("Please enter your cellphone model now. ")
         debug_mode = input("Whether to open the debug mode? (y/n)")
         if debug_mode in ['y', 'Y']:
-            debug_mode = 'True'
+            debug_mode = True
         else:
-            debug_mode = 'False'
-        with open('profile/default.txt', 'w') as f:
-            f.write(phone_model)
-            f.write(' ')
-            f.write(debug_mode)
-
-    with open('profile/' + phone_model + '.txt', 'r') as f:
-        coefficient = float(f.read())
-    print("Current configuration: " + phone_model)
-    print("Debug mode: " + debug_mode)
-    print("Profile: profile/default.txt")
+            debug_mode = False
+        with open('profile/default.json', 'w') as f:
+            configuration = {"cellType": phone_model, "dedug": debug_mode}
+            json.dump(configuration, f)
+    try :
+        with open('profile/{cellPhone}.json'.format(cellPhone = phone_model), 'r') as f:
+            configuration = json.load(f)
+            resolution = configuration["resolution"]
+            coefficient= configuration["coefficient"] 
+    except FileNotFoundError:
+        print("Please confirm \"default.json\" is right or contact the provider. ")
+    with open('profile/{resolution}.json'.format(resolution = resolution), 'r') as f:
+        configuration = json.load(f)
+        scale = configuration["scale"]
+    print("Current configurationuration: " + phone_model)
+    print("Debug mode: " + str(debug_mode))
+    print("Profile: profile/default.json")
     # Determine the configuration of cellphone model
     # Whether to open the debug mode
 
     while True and out:
-        out = adb_utils.adb_sc()  # screenshots
+        out, img = adb_utils.adb_sc()  # screenshots
         if out:
             print("Wait...")
-            # platform_position = 0
-            img, avatar_img, edge_img = img_proc.edge_detection()
+            img,  edge_img = img_proc.edge_detection(img = img, debug_mode = debug_mode)
 
-            avatar_position, top_left, bottom_right = img_proc.find_avatar(img, avatar_img)
+            avatar_position, top_left, bottom_right = img_proc.find_avatar(img, avatar_img, scale)
             if avatar_position:
                 if avatar_position[1] < img.shape[1] / 2:
                     # if avatar on the left, scanning right
@@ -59,12 +68,12 @@ def main():
                 img[platform_position[0], platform_position[1]] = (225, 0, 0)
                 cv2.rectangle(img, (top_left[1], top_left[0]), (bottom_right[1], bottom_right[0]), (255, 255, 255), 2)
                 # mark the position in the image
-                if debug_mode == 'True':
+                if debug_mode:
                     cv2.imwrite('images/center_{}.png'.format(i), img)
-                i += 1
+                    i += 1
                 dis = np.sqrt((avatar_position[0] - platform_position[0]) ** 2 + (avatar_position[1] - platform_position[1]) ** 2)
-                adb_utils.adb_touch(int(coefficient * dis))
-                time.sleep(random.uniform(1.2, 1.4))
+                adb_utils.adb_touch(int(coefficient * dis), h1 = avatar_position[0], x1 = avatar_position[1], h2 = platform_position[0], x2 = platform_position[1])
+        time.sleep(random.uniform(1.2, 1.4))
 
 
 if __name__ == '__main__':
